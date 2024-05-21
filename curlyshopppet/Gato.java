9ff30a9cd461fd99833fb4d8ac88ecd27b89e5e6
victor <victorferrero1303@gmail.com>
2024-05-21 11:09:13 +0200

    creado el proyecto de la tienda de mascotas

---
+++ curlyshopppet/Gato.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Gato here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Gato
+{
+    // instance variables - replace the example below with your own
+    private int edad;
+    private String color;
+
+    /**
+     * Constructor for objects of class Gato
+     */
+    public Gato()
+    {
+      
+    }
+
+}
---
+++ curlyshopppet/Tienda.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Tienda here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tienda
+{
+    // instance variables - replace the example below with your own
+    private String localizacion;
+    private boolean abierto;
+    private Gato gato;
+
+    /**
+     * Constructor for objects of class Tienda
+     */
+    public Tienda()
+    {
+        
+       
+    }
+
+}
